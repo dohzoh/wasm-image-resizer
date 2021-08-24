e5438796886e3bf7f0b7e5a137aa843357da4254
dozo <dozo@DzWorks.localdomain>
2021-08-24 22:46:40 +0900

    fixed size

--- src/components/ResizeImageCV.svelte
+++ src/components/ResizeImageCV.svelte
@@ -20,7 +20,7 @@
 		console.timeEnd("create dense array");
 
 		console.time("cv::resize()");
-		let dsize = new cv.Size(300, 300);
+		let dsize = new cv.Size(512, 512);
 		// You can try more different parameters
 		cv.resize(src, dst, dsize, 0, 0, cv.INTER_AREA);
 		console.timeEnd("cv::resize()");
