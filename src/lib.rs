25fe41367fbf61e92084cfde5adcf5d64f82302e
dozo <dozo@DzWorks.localdomain>
2021-09-01 22:53:06 +0900

    remove photon

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ extern crate wasm_bindgen;
 
 use image::*;
 use js_sys::*;
-use photon_rs::*;
 use wasm_bindgen::prelude::*;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, ImageData};
 
 #[wasm_bindgen]
 extern "C" {
@@ -16,9 +16,7 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn wasm_run() {
-    let _res = photon_rs::run();
-}
+pub fn wasm_run() {}
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
@@ -27,28 +25,45 @@ pub fn add(a: i32, b: i32) -> i32 {
 
 #[wasm_bindgen]
 pub fn resize_image(
-    canvas: web_sys::HtmlCanvasElement,
-    ctx: web_sys::CanvasRenderingContext2d,
+    canvas: HtmlCanvasElement,
+    ctx: CanvasRenderingContext2d,
     width: usize,
     height: usize,
     fmt: &str,
 ) -> Uint8Array {
     console_error_panic_hook::set_once();
 
-    time("photon_rs::open_image in Rust");
-    let photon_image = photon_rs::open_image(canvas, ctx);
-    timeEnd("photon_rs::open_image in Rust");
+    time("get_image_data in Rust");
+    time("replace image::load_from_memory() in Rust");
+    let image_width = canvas.width();
+    let image_height = canvas.height();
+
+    let data: ImageData = ctx
+        .get_image_data(0.0, 0.0, image_width as f64, image_height as f64)
+        .unwrap();
+    timeEnd("get_image_data in Rust");
+    time("data.data() in Rust");
+    let image_data = data.data().to_vec();
+    timeEnd("data.data() in Rust");
 
     // バッファから画像を読み込む
-    time("helpers::dyn_image_from_raw in Rust");
-    let dynamic_image = helpers::dyn_image_from_raw(&photon_image);
-    timeEnd("helpers::dyn_image_from_raw in Rust");
+    time("ImageBuffer::from_vec in Rust");
+    let img_buffer = ImageBuffer::from_vec(image_width, image_height, image_data).unwrap();
+    timeEnd("ImageBuffer::from_vec in Rust");
+    time("DynamicImage::ImageRgba8 in Rust");
+    let dynamic_image = DynamicImage::ImageRgba8(img_buffer);
+    timeEnd("DynamicImage::ImageRgba8 in Rust");
+    timeEnd("replace image::load_from_memory() in Rust");
 
     // 指定サイズに画像をリサイズする
-    time("image::resize_exact() in Rust");
-    let resized =
-        dynamic_image.resize_exact(width as u32, height as u32, imageops::FilterType::Triangle);
-    timeEnd("image::resize_exact() in Rust");
+    time("image::imageops::resize() in Rust");
+    let resized = DynamicImage::ImageRgba8(image::imageops::resize(
+        &dynamic_image,
+        width as u32,
+        height as u32,
+        imageops::FilterType::Nearest,
+    ));
+    timeEnd("image::imageops::resize() in Rust");
 
     // バッファに画像を書き出す
     time("save_to_buffer in Rust");
